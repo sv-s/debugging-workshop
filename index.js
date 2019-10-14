8d71b0c4944679911eda08581e9220263b703f1e
github username <github email address>
2019-10-14 15:34:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
-  const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
-  let joke;
+  const jokeList = document.getElementById('joke-list')  
 
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+    return fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
     })
-    .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
   }
 
   form.addEventListener('submit', (event) => {
-
-    if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+    let username = document.getElementById('name-input').value
+    event.preventDefault();
+    if (username !== "") {
+      fetchJoke()
+      .then(res => res.json())
+      .then(jokeData => {
+        let joke = jokeData.joke
+        let newJokeLi = document.createElement('li')
+        newJokeLi.innerHTML = `
+          <span class="username">${username} says:</span> ${joke}
+        `
+        jokeList.appendChild(newJokeLi)
+      });
+    }
   })
 })
